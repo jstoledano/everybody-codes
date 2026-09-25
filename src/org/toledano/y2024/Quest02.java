f60128092884a0344bb67e5621be8e5d6a92a14e
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2026-09-25 15:31:47 -0600

    Working in 2024.Quest02.Part2

--- src/org/toledano/y2024/Quest02.java
+++ src/org/toledano/y2024/Quest02.java
@@ -7,22 +7,46 @@ public class Quest02 {
     public static int part1(String input) {
         int runicWordsCount = 0;
 
-        var nonEmptyInput = input
+        var cleanedInput = input
             .lines()
             .filter(line -> !line.isBlank())
             .collect(Collectors.toList());
-        var words = nonEmptyInput.get(0).substring("WORDS:".length()).split(",");
-        var runicWords = nonEmptyInput.get(1);
+        var words = cleanedInput.get(0).substring("WORDS:".length()).split(",");
+        var runicWords = cleanedInput.get(1);
 
-        for (var word :  words) {
+        for (var word : words) {
             int index = 0;
             while ((index = runicWords.indexOf(word, index)) != -1) {
                 runicWordsCount++;
                 index++;
             }
         }
-        
+
         return runicWordsCount;
     }
+
+    public static int part2(String input) {
+        var runicSymbolsCount = 0;
+
+        var cleanedInput = input.lines()
+            .filter(line -> !line.isBlank())
+                    .collect(Collectors.toList());
+
+        var words = cleanedInput.get(0).substring("WORDS:".length()).split(",");
+        var inscriptionLines = cleanedInput.subList(1, cleanedInput.size());
+
+        for (var line : inscriptionLines){
+            for (var word : words) {
+                int index = 0;
+                while ((index = line.indexOf(word, index)) != -1) {
+                    runicSymbolsCount += word.length();
+                    index++;
+                }
+            }
+        }
+
+        
+       return runicSymbolsCount;
+    } 
 
 }
--- test/org/toledano/y2024/Quest02Test.java
+++ test/org/toledano/y2024/Quest02Test.java
@@ -8,12 +8,26 @@ class Quest02Test {
 
     @Test
     void part1_sample_returns_4() {
-        String input="""
-WORDS:THE,OWE,MES,ROD,HER
+        String input = """
+                WORDS:THE,OWE,MES,ROD,HER
 
-AWAKEN THE POWER ADORNED WITH THE FLAMES BRIGHT IRE
-""";
+                AWAKEN THE POWER ADORNED WITH THE FLAMES BRIGHT IRE
+                """;
         assertEquals(4, Quest02.part1(input));
     }
-    
+
+    @Test
+    void part2_sample_full_inscription_returns_42() {
+        var input = """
+            WORDS:THE,OWE,MES,ROD,HER,QAQ
+            
+            AWAKEN THE POWE ADORNED WITH THE FLAMES BRIGHT IRE
+            THE FLAME SHIELDED THE HEART OF THE KINGS
+            POWE PO WER P OWE R
+            THERE IS THE END
+            QAQAQ
+            """;
+        
+        assertEquals(42, Quest02.part2(input));
+    }
 }
